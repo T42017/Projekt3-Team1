f5ebbae90e0a1a1345e3a5a1dba10b4ca5e1c5ec
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-10-09 10:29:01 +0200

    Initial Commit

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+	
+<tr><th>Id</th><th>Email</th><th>Telephonenumber</th><th>Name</th><th>Description</th><th>Picture</th><th>Prive</th><th>Date</th></tr>	
+<table>
+	
+<?php
+	
+	
+
+	
+?>	
+</table>
+
+
+	
+</body>
+</html>
