64f9435a507e2613acd3a8da3d452abe09d75382
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-10-09 11:42:54 +0200

    Förbättrade filtreringsmetoden

--- Index.php
+++ Index.php
@@ -16,31 +16,41 @@
 
     <tr> <td>Email</td> <td>Telephone</td> <td>Name</td> <td>Title</td> <td>Category</td> <td>Description</td> <td>Picture</td> <td>Price</td> <td>Date Of Upload </td> </tr>
     <?php
-  
-	if(isset($_GET['email']))
-{
-	$continent = $_GET['email'];
-	$statement  = $db->prepare('SELECT * FROM annons WHERE Email=:email ORDER BY date DESC');
-	$statement ->bindParam(':continent', $continent);
-	$statement ->execute();
-}else
-{
-	$statement  = $db->query('SELECT * FROM annons ORDER BY date Desc');
-}
 
+    if(isset($_GET['email']))
+    {
+        $email = $_GET['email'];
+
+    }
+    else
+    {
+        $email = null;
+    }if(isset($_GET['name']))
+    {
+        $name = $_GET['name'];
+
+    }
+    else
+    {
+        $name = null;
+    }
+
+    $statement  = $db->prepare("SELECT * FROM annons WHERE Email LIKE '%$email%' AND Name Like '%$name%' ORDER BY date DESC");
+    $statement ->bindParam(':email', $email);
+    $statement ->execute();
 	
 	
     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
         echo ("<tr>");
-	     echo '<td>'.$row['email'].'</td>';
-	    echo '<td>'.$row['telnr'].'</td>';
-	    echo '<td>'.$row['name'].'</td>';
-	    echo '<td>'.$row['title'].'</td>';
-	    echo '<td>'.$row['category'].'</td>';
-	    echo '<td>'.$row['description'].'</td>';
-	    echo '<td>'.$row['picture'].'</td>';
-	    echo '<td>'.$row['price'].'</td>';
-         echo '<td>'.$row['date'].'</td>';
+        echo "<td>{$row['email']}</td>";
+        echo '<td>'.$row['telnr'].'</td>';
+        echo '<td>'.$row['name'].'</td>';
+        echo '<td>'.$row['title'].'</td>';
+        echo '<td>'.$row['category'].'</td>';
+        echo '<td>'.$row['description'].'</td>';
+        echo '<td>'.$row['picture'].'</td>';
+        echo '<td>'.$row['price'].'</td>';
+        echo '<td>'.$row['date'].'</td>';
 
 
     }
