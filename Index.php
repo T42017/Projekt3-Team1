83618c3b6d36410bb781b429aa7a2eabfc054fb9
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-10-10 13:26:00 +0200

    Lade till en sida där man kan se mer info om den valda annonsen

--- Index.php
+++ Index.php
@@ -5,6 +5,29 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
+<?php
+
+function GetPermaLink($skip = 0)
+{
+    $path = ltrim($_SERVER['REQUEST_URI'], '/');
+    $elements = explode('/', $path);
+
+    if(empty($elements[0]))
+    {
+        return null;
+    }
+    else
+    {
+        for($i=0; $i< $skip;$i++)
+            array_shift($elements);
+
+        $req = array_shift($elements);
+        return strtolower($req);
+    }
+}
+
+?>
+
 <head>
 
 
@@ -14,7 +37,7 @@
 
 <table>
 
-    <tr> <td>Email</td> <td>Telephone</td> <td>Name</td> <td>Title</td> <td>Category</td> <td>Description</td> <td>Picture</td> <td>Price</td> <td>Date Of Upload </td> </tr>
+    <tr> <td>Title</td> <td>Telephone</td> <td>Name</td> <td>Title</td> <td>Category</td> <td>Description</td> <td>Picture</td> <td>Price</td> <td>Date Of Upload </td> </tr>
     <?php
 
     if(isset($_GET['email']))
@@ -25,7 +48,9 @@
     else
     {
         $email = null;
-    }if(isset($_GET['name']))
+    }
+
+    if(isset($_GET['name']))
     {
         $name = $_GET['name'];
 
@@ -35,27 +60,48 @@
         $name = null;
     }
 
-    $statement  = $db->prepare("SELECT * FROM annons WHERE Email LIKE '%$email%' AND Name Like '%$name%' ORDER BY date DESC");
+    if(isset($_GET['title']))
+    {
+        $title = $_GET['title'];
+
+    }
+    else
+    {
+        $title = null;
+    }
+
+    if(isset($_GET['category']))
+    {
+        $category = $_GET['category'];
+
+    }
+    else
+    {
+        $category = null;
+    }
+
+    $statement  = $db->prepare("SELECT * FROM annons WHERE Email LIKE '%$email%' 
+                                AND Name LIKE '%$name%' 
+                                AND Title LIKE '%$title%'
+                                AND Category LIKE '%$category%'                              
+                                ORDER BY date DESC");
     $statement ->bindParam(':email', $email);
     $statement ->execute();
 	
-	
     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-        echo ("<tr>");
+        $id = "annons/?id=".$row['ID'];
+        echo "<tr>";
+        echo "<td><a href='{$id}'>{$row['title']}</a></td>";
         echo "<td>{$row['email']}</td>";
         echo '<td>'.$row['telnr'].'</td>';
         echo '<td>'.$row['name'].'</td>';
-        echo '<td>'.$row['title'].'</td>';
         echo '<td>'.$row['category'].'</td>';
         echo '<td>'.$row['description'].'</td>';
         echo '<td>'.$row['picture'].'</td>';
         echo '<td>'.$row['price'].'</td>';
         echo '<td>'.$row['date'].'</td>';
-
-
     }
-	
-	
+
     echo ("</tr>");
     ?>
 </table>
---
+++ annons.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<meta charset = "utf-8">
+<?php $db = new PDO('mysql:host=localhost;dbname=annonser;charset=utf8mb4','root', '');?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+<?php
+
+function GetPermaLink($skip = 0)
+{
+    $path = ltrim($_SERVER['REQUEST_URI'], '/');
+    $elements = explode('/', $path);
+
+    if(empty($elements[0]))
+    {
+        return null;
+    }
+    else
+    {
+        for($i=0; $i< $skip;$i++)
+            array_shift($elements);
+
+        $req = array_shift($elements);
+        return strtolower($req);
+    }
+}
+
+if(isset($_GET['id']))
+{
+    $id = $_GET['id'];
+}
+else {
+    echo ddd;
+}
+?>
+<body>
+<table>
+
+<tr> <td>Title</td> <td>Telephone</td> <td>Name</td> <td>Title</td> <td>Category</td> <td>Description</td> <td>Picture</td> <td>Price</td> <td>Date Of Upload </td> </tr>
+
+<?php
+
+$statement  = $db->prepare("SELECT * FROM annons WHERE ID LIKE '%$id%'");
+$statement ->bindParam(':email', $email);
+$statement ->execute();
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+    echo "<tr>";
+    echo "<td>{$row['title']}</td>";
+    echo "<td>{$row['email']}</td>";
+    echo '<td>'.$row['telnr'].'</td>';
+    echo '<td>'.$row['name'].'</td>';
+    echo '<td>'.$row['category'].'</td>';
+    echo '<td>'.$row['description'].'</td>';
+    echo '<td>'.$row['picture'].'</td>';
+    echo '<td>'.$row['price'].'</td>';
+    echo '<td>'.$row['date'].'</td>';
+}
+
+?>
+
+
+
+
+</table>
+    </body>
