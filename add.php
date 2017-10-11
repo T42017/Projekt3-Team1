df6dacd625124733b7522df5cfe6caa5fec1a830
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-10-11 10:03:42 +0200

    försöker fixa mergen

--- add.php
+++ add.php
@@ -6,6 +6,7 @@ $db = new PDO('mysql:host=localhost;dbname=annonser;charset=utf8mb4','root', '')
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
 
