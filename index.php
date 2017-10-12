89d9e6cd27f6fd822ecdce87d19da380ab7838e0
OskarMolander <oskar.molander@lund.ntig.se>
2017-10-12 15:20:30 +0200

    index partially structured. (only visuals, no features)

--- index.php
+++ index.php
@@ -5,85 +5,71 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<script src="javascript.js"></script>
 
 
 <head>
-
-
 </head>
 <body>
 <div id="main">
+        <header id="header">
+            <div class="max">
+                <div class="logo">
+                    <h1><a href="#">Plocket</a></h1>
+                </div>
+                <form class="search" action="index.php">
+                    <input class="searchTerm" placeholder="Enter your search term ..."><input class="searchButton" type="submit">
+                </form>
+                <nav class="section" id="nav"><a href="#"><h5>Add</a><a href="#">Login</a><a href="#">Support</a></h5></nav>
+            </div>
+        </header> 
+        
+            <div class="sidebar"> 
+            <br> <b> Categories </b>
+            <br>
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            <br> Pars & Accessories
+            </div>
+           
+
+            <div id="content"> 
+            <div class="article">
+                 Title &emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; description
+                <br><br><br><br> Bild
+            </div>
+            <div class="article">
+                 Title &emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; description
+                <br><br><br><br> Bild
+            </div>
+            <div class="article">
+                 Title &emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; description
+                <br><br><br><br> Bild
+            </div>
+            <div class="article">
+                 Title &emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; description
+                <br><br><br><br> Bild
+            </div>
+            <div class="article">
+                 Title &emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; description
+                <br><br><br><br> Bild
+            </div>       
+            </div>
+
     
-       
-    <nav class="navbar">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#"><h1 >LOGO/TITEL</h1></a>
-        </div>
-        <ul class="nav">
-          <li class="active"><a href="#"> Home </a></li>
-          <li><a href="#">Page 1</a></li>
-          <li><a href="#">Page 2</a></li>
-          <li><a href="#">Page 3</a></li>
-        </ul>
-
-      </div>
-    </nav>
-    <form class="search" action="index.php">
-            <input class="searchTerm" placeholder="Enter your search term ..."><input class="searchButton" type="submit">
-    </form>
-
-    <div id="content">
-        <div class="well">  
-            <table class="table">
-                <tr> <td>Email</td> <td>Telephone</td> <td>Name</td> <td>Title</td> <td>Category</td> <td>Description</td> <td>Picture</td> <td>Price</td> <td>Date Of Upload </td> </tr>
-                <?php
-                
-                if(isset($_GET['email']))
-                {
-                    $email = $_GET['email'];
-
-                }
-                else
-                {
-                    $email = null;
-                }if(isset($_GET['name']))
-                {
-                    $name = $_GET['name'];
-
-                }
-                else
-                {
-                    $name = null;
-                }
-
-                $statement  = $db->prepare("SELECT * FROM annons WHERE Email LIKE '%$email%' AND Name Like '%$name%' ORDER BY date DESC");
-                $statement ->bindParam(':email', $email);
-                $statement ->execute();
-            	
-            	
-                while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-                    echo ("<tr>");
-                    echo "<td>{$row['email']}</td>";
-                    echo '<td>'.$row['telnr'].'</td>';
-                    echo '<td>'.$row['name'].'</td>';
-                    echo '<td>'.$row['title'].'</td>';
-                    echo '<td>'.$row['category'].'</td>';
-                    echo '<td>'.$row['description'].'</td>';
-                    echo '<td>'.$row['picture'].'</td>';
-                    echo '<td>'.$row['price'].'</td>';
-                    echo '<td>'.$row['date'].'</td>';
-                }
-                echo ("</tr>");
-                ?>
-            </table>
-        </div>
-        <div class="well">
-        <footer> footer</footer>
-        </div>
-    </div>	
+    <footer> 
+        <h2> footer </h2>
+    </footer>     	
 </div>
-
-
-
 </body>
+</html>
---
+++ phpkod.php
@@ -0,0 +1,44 @@
+<table class="table">
+                <tr> <td>Email</td> <td>Telephone</td> <td>Name</td> <td>Title</td> <td>Category</td> <td>Description</td> <td>Picture</td> <td>Price</td> <td>Date Of Upload </td> </tr>
+                <?php
+                /*
+                if(isset($_GET['email']))
+                {
+                    $email = $_GET['email'];
+
+                }
+                else
+                {
+                    $email = null;
+                }if(isset($_GET['name']))
+                {
+                    $name = $_GET['name'];
+
+                }
+                else
+                {
+                    $name = null;
+                }
+
+                $statement  = $db->prepare("SELECT * FROM annons WHERE Email LIKE '%$email%' AND Name Like '%$name%' ORDER BY date DESC");
+                $statement ->bindParam(':email', $email);
+                $statement ->execute();
+                
+                
+                while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+                    echo ("<tr>");
+                    echo "<td>{$row['email']}</td>";
+                    echo '<td>'.$row['telnr'].'</td>';
+                    echo '<td>'.$row['name'].'</td>';
+                    echo '<td>'.$row['title'].'</td>';
+                    echo '<td>'.$row['category'].'</td>';
+                    echo '<td>'.$row['description'].'</td>';
+                    echo '<td>'.$row['picture'].'</td>';
+                    echo '<td>'.$row['price'].'</td>';
+                    echo '<td>'.$row['date'].'</td>';
+                }
+                echo ("</tr>");
+                */
+                ?>
+
+</table>-->
